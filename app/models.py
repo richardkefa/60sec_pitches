889a12d736dfed12ac0c80a7eaa3fe67907b8689
richardkefa <richardkefa@gmail.com>
2020-09-20 18:15:40 +0300

    add pitch, coments and category class

--- app/models.py
+++ app/models.py
@@ -5,7 +5,29 @@ class User(db.Model):
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(255))
   email = db.Column(db.String(255),unique = True,index = True)
+  pitch_id = db.relationship('Pitch',backref = 'user',lazy="dynamic")
   
+class Pitch(db.Model):
+  __tablename_ = 'pitches'
+  id = db.Column(db.Integer,primary_key = True)
+  pitch = db.Column(db.String(255))
+  upvote = db.Column(db.Integer)
+  downvote = db.Column(db.Integer)
+  user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+  comments = db.relationship('Comment',backref = 'pitch',lazy="dynamic")
+  category = db.relationship('Category',backref = 'pitch',lazy="dynamic")
+  
+class Comment(db.Model):
+  __tablename_ = 'comments'
+  id = db.Column(db.Integer,primary_key = True)
+  comment = db.Column(db.String(255))
+  pitch_id = db.Colmun(db.Integer,db.ForeignKey('pitches.id'))
+  
+class Category(db.Model):
+  __tablename_ = 'categories'
+  id = db.Column(db.Integer,primary_key = True)
+  category = db.Column(db.String(255))
+  pitch_id = db.Colmun(db.Integer,db.ForeignKey('pitches.id'))
   
   def __repr__(self):
     return f'User {self.username}'
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import db,create_app
-from app.models import User
+from app.models import User,Pitch,Comment,Category
 
 app = create_app('development')
 
 @manager.shell
 def make_shell_context():
-  return dict(app = app,db = db,User = User)
+  return dict(app = app,db = db,User = User,Pitch = Pitch,Comment = Comment,Category = Category)
 if __name__ == "__main__":
     manager.run()
