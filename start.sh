c81d745d4cf3a99dbbf08e78c4903390863606ee
richardkefa <richardkefa@gmail.com>
2020-09-29 15:20:35 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY='FOI894UT834U'
 export MAIL_USERNAME='richardkefa7@gmail.com'
 export MAIL_PASSWORD='kefa1887'
-export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost/pitchit'
+export DATABASE_URL='postgresql+psycopg2://moringa:Access@localhost/pitchit?sslmode=require'
 
 python3.6 manage.py server
